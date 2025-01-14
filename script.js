179dea2199f301f3dd56f5a50e2d013b08740c05
andreacagnina <amirkal@libero.it>
2025-01-14 20:49:38 +0100

    [feat] completed

--- script.js
+++ script.js
@@ -5,6 +5,39 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+const allCategories = {
+    299: { category: "Career Advice", className: "career" },
+    409: { category: "Project Feedback", className: "feedback" },
+    417: { category: "freeCodeCamp Support", className: "support" },
+    421: { category: "JavaScript", className: "javascript" },
+    423: { category: "HTML - CSS", className: "html-css" },
+    424: { category: "Python", className: "python" },
+    432: { category: "You Can Do This!", className: "motivation" },
+    560: { category: "Backend Development", className: "backend" },
+};
+
+const forumCategory = (id) => {
+    let selectedCategory = {};
+
+    if (allCategories.hasOwnProperty(id)) {
+        const { className, category } = allCategories[id];
+
+        selectedCategory.className = className;
+        selectedCategory.category = category;
+    } else {
+        selectedCategory.className = "general";
+        selectedCategory.category = "General";
+        selectedCategory.id = 1;
+    }
+    const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
+    const linkText = selectedCategory.category;
+    const linkClass = `category ${selectedCategory.className}`;
+
+    return `<a href="${url}" class="${linkClass}" target="_blank">
+    ${linkText}
+  </a>`;
+};
+
 const timeAgo = (time) => {
     const currentTime = new Date();
     const lastPost = new Date(time);
@@ -26,6 +59,32 @@ const timeAgo = (time) => {
 
     return `${daysAgo}d ago`;
 };
+
+const viewCount = (views) => {
+    const thousands = Math.floor(views / 1000);
+
+    if (views >= 1000) {
+        return `${thousands}k`;
+    }
+
+    return views;
+};
+
+const avatars = (posters, users) => {
+    return posters
+        .map((poster) => {
+            const user = users.find((user) => user.id === poster.user_id);
+            if (user) {
+                const avatar = user.avatar_template.replace(/{size}/, 30);
+                const userAvatarUrl = avatar.startsWith("/user_avatar/")
+                    ? avatarUrl.concat(avatar)
+                    : avatar;
+                return `<img src="${userAvatarUrl}" alt="${user.name}" />`;
+            }
+        })
+        .join("");
+};
+
 const fetchData = async () => {
     try {
         const res = await fetch(forumLatest);
@@ -56,12 +115,15 @@ const showLatestPosts = (data) => {
 
         return `
     <tr>
-      <td>
-        <p class="post-title">${title}</p>
+      <td>        <a class="post-title" target="_blank" href="${forumTopicUrl}${slug}/${id}">${title}</a>    ${forumCategory(category_id)}
       </td>
-      <td></td>
+      <td>
+        <div class="avatar-container">
+          ${avatars(posters, users)}
+        </div>
+      </td>
       <td>${posts_count - 1}</td>
-      <td>${views}</td>
+      <td>${viewCount(views)}</td>
       <td>${timeAgo(bumped_at)}</td>
     </tr>`;
     }).join("");
